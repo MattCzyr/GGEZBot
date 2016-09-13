c4529eaa71b14fef17a67a74045122d623135e4f
ChaosTheDude <chaosthedude@gmail.com>
2016-09-12 22:29:08 -0400

    Alphabetized responses and realized I had one in the list twice. Oops.

--- GGEZBot.py
+++ GGEZBot.py
@@ -7,27 +7,26 @@ from praw.helpers import comment_stream
 
 USERNAME = input('Username: ')
 PASSWORD = input('Password: ')
-USERAGENT = "GG EZ v1.1.0 (by /u/ChaosTheDude)"
+USERAGENT = "GG EZ v1.1.1 (by /u/ChaosTheDude)"
 
 r = praw.Reddit(USERAGENT)
 r.login(USERNAME, PASSWORD)
 
 FOOTER = "\n\n---\n^I ^am ^a ^bot ^made ^by ^/u/ChaosTheDude. ^Beep ^boop. ^| ^[GitHub](https://github.com/ChaosTheDude/GG-EZ-Bot)"
 
-RESPONSES = ['Well played. I salute you all.',
-             'For glory and honor! Huzzah comrades!',
-             'I\'m wrestling with some insecurity issues in my life but thank you all for playing with me.',
-             'It\'s past my bedtime. Please don\'t tell my mommy.',
-             'Gee whiz! That was fun. Good playing!',
-             'I feel very, very small... please hold me...',
-             'Ah shucks... you guys are the best!',
-             'Good game! Best of luck to you all!',
-             'I\'m trying to be a nicer person. It\'s hard, but I am trying, guys.',
-             'Mommy says people my age shouldn\'t suck their thumbs.',
-             'Well played. I salute you all.',
+RESPONSES = ['Ah shucks... you guys are the best!',
              'C\'mon, Mom! One more game before you tuck me in. Oops mistell.',
+             'For glory and honor! Huzzah comrades!',
+             'Gee whiz! That was fun. Good playing!',
+             'Good game! Best of luck to you all!',
              'Great game, everyone!',
-             'It was an honor to play with you all. Thank you.']
+             'I feel very, very small... please hold me...',
+             'It was an honor to play with you all. Thank you.'
+             'It\'s past my bedtime. Please don\'t tell my mommy.',
+             'I\'m trying to be a nicer person. It\'s hard, but I am trying, guys.',
+             'I\'m wrestling with some insecurity issues in my life but thank you all for playing with me.',
+             'Mommy says people my age shouldn\'t suck their thumbs.',
+             'Well played. I salute you all.']
 
 processed = set()
 
